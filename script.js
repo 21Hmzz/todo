ea36914901ec4a9c7ec91f1cf5b8a36d122dab80
Hamza Bella <hamza@stevegates.co>
2025-02-24 12:19:46 +0100

    init des éléments requis en js et en html

---
+++ script.js
@@ -0,0 +1,7 @@
+const taskForm = document.getElementById("task-form");
+const taskInput = document.getElementById("task-input");
+const taskList = document.getElementById("task-list");
+
+console.log(taskForm);
+console.log(taskInput);
+console.log(taskList);
